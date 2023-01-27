fe37db0dc55d04c53546cb6293141d684204ba5f
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-27 20:19:37 +0530

    Updated Indian States Census Analyser Problem!!

---
+++ Indian_States_Census_analyser-main/Indian_States_Census_analyser-main/CensusAnalyserProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Indian States Census Analyser program \n");
+        }
+    }
+}
